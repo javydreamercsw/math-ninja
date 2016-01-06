835dcaf6cc2b95e8ac2eff2292ce840df307cfa1
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2016-01-06 14:52:06 -0600

    Added hidden way of setting current level.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -5,16 +5,12 @@ using System.Collections;
 public class Settings : MonoBehaviour
 {
 	public Toggle ones, zeroes;
+	public Dropdown level;
 	// Use this for initialization
 	void Awake ()
 	{
 		ones.isOn = PlayerPrefs.GetInt ("ones") == 1;
 		zeroes.isOn = PlayerPrefs.GetInt ("zeroes") == 1;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
 	}
 
 	public void back ()
@@ -35,5 +31,15 @@ public class Settings : MonoBehaviour
 		PlayerPrefs.SetInt ("zeroes", zeroes.isOn ? 1 : 0);
 		Debug.Log ("zero=" + PlayerPrefs.GetInt ("zeroes"));
 		PlayerPrefs.Save ();
+	}
+
+	public void updateLevel ()
+	{
+		PlayerPrefs.SetInt (GameControl.LEVEL, level.value + 1);
+	}
+
+	public void toggle ()
+	{
+		level.enabled = !level.enabled;
 	}
 }
