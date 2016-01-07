26827327c98db763a195f98d9ab76a35574957be
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2016-01-07 22:38:05 +0000

    Created new branch MN-3-typo

---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class NewPlayer : MonoBehaviour {
+	public InputField input;
+
+	// Update is called once per frame
+	void Update () {
+		PlayerPrefs.SetString (GameControl.NAME, input.text);
+	}
+
+	public void next(){
+		PlayerPrefs.SetInt (GameControl.LEVEL, 0);
+		GameControl.LoadLevel ("Welcome");
+	}
+}
