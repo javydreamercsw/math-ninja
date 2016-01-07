a58e912ee93c00b05a3a8d5889845d0e6ba1fbff
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2016-01-07 22:49:45 +0000

    Created new branch MN-2

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Settings : MonoBehaviour
+{
+	public Toggle ones, zeroes;
+	public Dropdown level;
+	// Use this for initialization
+	void Awake ()
+	{
+		ones.isOn = PlayerPrefs.GetInt ("ones") == 1;
+		zeroes.isOn = PlayerPrefs.GetInt ("zeroes") == 1;
+		level.value = PlayerPrefs.GetInt (GameControl.LEVEL) - 1;
+		level.enabled=false;
+	}
+
+	public void back ()
+	{
+		GameControl.LoadLevel ("Main");
+		PlayerPrefs.Save ();
+	}
+
+	public void togleOnes ()
+	{
+		PlayerPrefs.SetInt ("ones", ones.isOn ? 1 : 0);
+		Debug.Log ("one=" + PlayerPrefs.GetInt ("ones"));
+		PlayerPrefs.Save ();
+	}
+
+	public void toggleZeroes ()
+	{
+		PlayerPrefs.SetInt ("zeroes", zeroes.isOn ? 1 : 0);
+		Debug.Log ("zero=" + PlayerPrefs.GetInt ("zeroes"));
+		PlayerPrefs.Save ();
+	}
+
+	public void updateLevel ()
+	{
+		PlayerPrefs.SetInt (GameControl.LEVEL, level.value + 1);
+	}
+
+	public void toggle ()
+	{
+		level.enabled = !level.enabled;
+	}
+}
