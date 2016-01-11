b8888a23e5eb3ecf789c6a93fa98a23a933b46fa
Javier A. Ortiz Bultron <javydreamercsw@users.noreply.github.com>
2016-01-11 12:33:15 -0600

    Add a donation button.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -21,4 +21,8 @@ public class Menu : MonoBehaviour {
 	public void Back(){
 		GameControl.LoadLevel ("Main");
 	}
+
+	public void donate(){
+		Application.OpenURL("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=5CYSFRLWSCD76&lc=US&item_name=Support%20Development&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted");
+	}
 }
