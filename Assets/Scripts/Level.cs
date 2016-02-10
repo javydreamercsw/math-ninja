a6ed1e7aaaa0537a9197a421e9dd1bf78ca44780
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2016-02-10 15:46:16 -0600

    Fix MN-7: was using wrong read method to obtain level information.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -11,19 +11,21 @@ public class Level : MonoBehaviour
 	void Awake ()
 	{
 		int level = PlayerPrefs.GetInt (GameControl.LEVEL);
-		Sprite s= GameControl.instance.getLevelSprite ();
+		if (level > 0) {
+			string[] levelInfo = PlayerPrefs.GetString ("level" + level).Split (',');
+			mess.text = "Your current belt is " + levelInfo [0];
+		} else {
+			mess.text = "Your current belt is white";
+		}
+		Debug.Log(mess.text);
+		Sprite s = GameControl.instance.getLevelSprite ();
 		if (s != null) {
-			if (level > 0) {
-				string[] levelInfo = GameControl.ReadFile ("level" + level);
-				mess.text = "Your current belt is " + levelInfo [0];
-			} else {
-				mess.text = "Your current belt is white";
-			}
 			image.sprite = s;
 		}
 	}
 
-	public void exit(){
+	public void exit ()
+	{
 		GameControl.LoadLevel ("Main");
 	}
 }
