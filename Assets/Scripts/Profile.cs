663a871e3940e9aa241491c46c501405619b794e
javydreamercsw <javydreamercsw@localhost>
2016-10-21 13:19:42 -0500

    Make name read only.

--- Assets/Scripts/Profile.cs
+++ Assets/Scripts/Profile.cs
@@ -2,17 +2,16 @@
 using UnityEngine.UI;
 
 public class Profile : MonoBehaviour {
-	public InputField playerName;
+	public Text playerName;
 	public Image mult, subs;
 
 	// Use this for initialization
 	void Awake() {
-		playerName.text = ProfileManager.getStringSetting(GameControl.NAME);
+		playerName.text = "Name: " + ProfileManager.getStringSetting(GameControl.NAME);
 	}
 
 	// Update is called once per frame
 	void Update() {
-		ProfileManager.setStringSetting(GameControl.NAME, playerName.text);
 		if (GameControl.instance != null) {
 			mult.sprite = GameControl.instance.getLevelSprite(Game.MODE.MULTIPLICATION);
 			subs.sprite = GameControl.instance.getLevelSprite(Game.MODE.SUBSTRACTION);
